9d0ebcd12af1351f9430030e5bd783809fcf2297
ez <ez@skippy.myfiosgateway.com>
2020-05-21 22:49:26 -0400

    remove antarctica, fix overlay paths &tools tips

--- src/WorldMap.tsx
+++ src/WorldMap.tsx
@@ -27,25 +27,26 @@ const CSizes: { [key: string]: number } = {
   "sm": 240,
   "md": 336,
   "lg": 480,
+  "xl": 640
 }
 
-const CViewBox = "0 -200 960 760"
-const CHeightRatio = 5/6
+const CViewBox = "0 -200 960 650"
+const CHeightRatio = 3 / 4
 
 export const WorldMap: React.FC<IProps> = (props) => {
 
   // Inits
-  const size = typeof (props.size) !== "undefined" ? props.size : "xs"
+  const size = typeof (props.size) !== "undefined" ? props.size : "sm"
   const width = CSizes[size]
   const height = CSizes[size] * CHeightRatio
-  const valuePrefix = (typeof(props.valuePrefix)==="undefined") ? "" : props.valuePrefix
-  const valueSuffix = (typeof(props.valueSuffix)==="undefined") ? "" : props.valueSuffix
-  const tooltipBgColor = (typeof(props.tooltipBgColor)==="undefined") ? "black" : props.tooltipBgColor
-  const tooltipTextColor = (typeof(props.tooltipTextColor)==="undefined") ? "white" : props.tooltipTextColor
-  const isBorder = (typeof(props.border)==="undefined") ? false : props.border
-  const borderColor = (typeof(props.borderColor)==="undefined") ? "black" : props.borderColor
-  const border = isBorder ? <rect x={0} y={-220} width={"100%"} height={"105%"} stroke={borderColor} fill="none"/> : <path></path>
-  const title = (typeof(props.title)==="undefined") ? "" : <p>{props.title}</p>
+  const valuePrefix = (typeof (props.valuePrefix) === "undefined") ? "" : props.valuePrefix
+  const valueSuffix = (typeof (props.valueSuffix) === "undefined") ? "" : props.valueSuffix
+  const tooltipBgColor = (typeof (props.tooltipBgColor) === "undefined") ? "black" : props.tooltipBgColor
+  const tooltipTextColor = (typeof (props.tooltipTextColor) === "undefined") ? "white" : props.tooltipTextColor
+  const isBorder = (typeof (props.border) === "undefined") ? false : props.border
+  const borderColor = (typeof (props.borderColor) === "undefined") ? "black" : props.borderColor
+  const border = isBorder ? <rect x={0} y={-200} width={"100%"} height={"100%"} stroke={borderColor} fill="none" /> : <path></path>
+  const title = (typeof (props.title) === "undefined") ? "" : <p>{props.title}</p>
 
   const containerRef = React.createRef<SVGSVGElement>();
 
@@ -61,48 +62,60 @@ export const WorldMap: React.FC<IProps> = (props) => {
     countryValueMap[key] = value
   })
 
-  // Build path for each country
+  // Build a path & a tooltip for each country
   const projection = geoMercator()
   const pathGenerator = geoPath().projection(projection)
 
-  const countriesPath = geoData.features
-    .map((feature, idx) => {
-      const triggerRef = React.createRef<SVGPathElement>();
-      const isHighlight = typeof (countryValueMap[feature.properties.ISO_A2]) != "undefined"
-      let color: string = CDefaultColor
-      let opacityLevel = 0.1
+  const pathsAndToolstips = geoData.features.map((feature, idx) => {
 
-      // Things to do if country is in data
-      if (isHighlight) {
-        color = props.color ? props.color : CDefaultColor
-        opacityLevel += (0.9 * (countryValueMap[feature.properties.ISO_A2] - min) / (max - min))
-      }
-      
-      const tooltip = (!isHighlight) ? "" :
-        <PathTooltip fontSize={24} bgColor={tooltipBgColor} textColor={tooltipTextColor} pathRef={triggerRef} svgRef={containerRef} tip={feature.properties.NAME + " " + valuePrefix + " " + countryValueMap[feature.properties.ISO_A2].toLocaleString() + " " + valueSuffix} />
+    const triggerRef = React.createRef<SVGPathElement>();
+    const isHighlight = typeof (countryValueMap[feature.properties.ISO_A2]) != "undefined"
+    let color: string = CDefaultColor
+    let opacityLevel = 0.2
 
-      // Build a path for a country
-      const path = (
-        <g key={feature.properties.NAME}>
-          <path
-            key={"path" + idx}
-            ref={triggerRef}
-            d={pathGenerator(feature as GeoJSON.Feature) as string}
-            style={{ fill: color, fillOpacity: opacityLevel, stroke: "black", strokeWidth: 1, strokeOpacity: 0.1, cursor: "pointer" }}
-          />
-          {tooltip}
-        </g>
-      )
-      return path
-    })
+    if (isHighlight) {
+      color = props.color ? props.color : CDefaultColor
+      opacityLevel += 0.2 + (0.6 * (countryValueMap[feature.properties.ISO_A2] - min) / (max - min))
+    }
+
+    const path = <path
+      key={"path" + idx}
+      ref={triggerRef}
+      d={pathGenerator(feature as GeoJSON.Feature) as string}
+      style={{ fill: color, fillOpacity: opacityLevel, stroke: "black", strokeWidth: 1, strokeOpacity: 0.2, cursor: "pointer" }}
+    />
+
+    const tooltip = (!isHighlight) ? <g></g> :
+      <PathTooltip
+        fontSize={12}
+        bgColor={tooltipBgColor}
+        textColor={tooltipTextColor}
+        pathRef={triggerRef}
+        svgRef={containerRef}
+        tip={feature.properties.NAME + " " + valuePrefix + " " + countryValueMap[feature.properties.ISO_A2].toLocaleString() + " " + valueSuffix}
+      />
+
+    return { "path": path, "tooltip": tooltip }
+  })
+
+  // build paths
+  const paths = pathsAndToolstips.map(entry => {
+    return entry.path
+  })
+
+  // build tooltips
+  const tooltips = pathsAndToolstips.map(entry => {
+    return entry.tooltip
+  })
 
   // Render the SVG
   return (
-    <div style={{backgroundColor: "white", height: "auto", width: "auto", padding: "0px", margin: "0px"}}>
+    <div style={{ backgroundColor: "white", height: "auto", width: "auto", padding: "0px", margin: "0px" }}>
       {title}
       <svg ref={containerRef} height={height + "px"} width={width + "px"} viewBox={CViewBox}>
         {border}
-        {countriesPath}
+        {paths}
+        {tooltips}
       </svg>
     </div>
   )
