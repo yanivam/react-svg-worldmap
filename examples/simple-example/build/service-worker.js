734891c998dc58cc9b7918425dd29f91d0580a55
ez <ez@skippy.myfiosgateway.com>
2020-05-25 15:42:08 -0400

    rebuild example

--- examples/simple-example/build/service-worker.js
+++ examples/simple-example/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ee0a4d3d87fe20983a1259a5142a74ba.js"
+  "/precache-manifest.18fb2cfdf8996477ed8daf6c46810b77.js"
 );
 
 self.addEventListener('message', (event) => {
