756e91f1a6049e0ada272bc444f617b97ba94421
Yaniv Amiri <yanivam123@gmail.com>
2020-08-22 14:44:03 -0400

    Package update

---
+++ examples/simple-example/build/precache-manifest.e5e7dbd5616d799a423c244331ec3c5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b25a4a3e4738ea2c8c12f9af4a38914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "372f13933137a576d092",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3eb4ee92ed2e73626b95",
+    "url": "/static/js/2.efabfbfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.efabfbfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372f13933137a576d092",
+    "url": "/static/js/main.1b0cf1e7.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
