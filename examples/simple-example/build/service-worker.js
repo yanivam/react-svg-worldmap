c6d8cde6b9c52429243a7c928d231dc42bca0f30
Yaniv Amiri <yanivam123@gmail.com>
2021-02-14 14:26:45 -0500

    Minify script works but still get error

--- examples/simple-example/build/service-worker.js
+++ examples/simple-example/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06c91265a8aca72beb9a6a48485bd955.js"
+  "/precache-manifest.b32b5618a1587a17e2ac6cce733fbeee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
