3ec7209a75c41e9876e1190bd47827c7d5909608
Joshua Chen <sidachen2003@gmail.com>
2021-08-26 13:32:38 +0800

    chore: test-build workflow (#92)
    * create test-build
    
    Signed-off-by: Josh-Cena <sidachen2003@gmail.com>
    
    * Aha
    
    Signed-off-by: Josh-Cena <sidachen2003@gmail.com>
    
    * Remove ls
    
    Signed-off-by: Josh-Cena <sidachen2003@gmail.com>
    
    * Add gitignore
    
    Signed-off-by: Josh-Cena <sidachen2003@gmail.com>

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/vsdark');
+const darkCodeTheme = require('prism-react-renderer/themes/vsDark');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
