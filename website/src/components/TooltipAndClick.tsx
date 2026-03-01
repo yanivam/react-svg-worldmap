f60f1ea82edc284511fb6a148e299fd9cc3fce8b
yanivam <yanivam123@gmail.com>
2026-02-28 21:16:08 -0500

    Fix smoke test issues

--- website/src/components/TooltipAndClick.tsx
+++ website/src/components/TooltipAndClick.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { useState } from "react";
 import type { CountryContext } from "react-svg-worldmap";
 import WorldMap from "react-svg-worldmap";
-import { populationData } from "../data/CountryData";
+import { populationData } from "../data/CountryData.js";
 
 /**
  * Repro page for tooltip + click interaction (Issue #148).
